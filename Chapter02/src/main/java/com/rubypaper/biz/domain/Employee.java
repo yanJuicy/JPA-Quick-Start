18a1e366a33c10fb27e08e2fa87469ac22c25046
Yanjoo <hanjub3@gmail.com>
2022-03-05 03:18:12 +0900

    @Column 속성

--- Chapter02/src/main/java/com/rubypaper/biz/domain/Employee.java
+++ Chapter02/src/main/java/com/rubypaper/biz/domain/Employee.java
@@ -6,30 +6,38 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 
 import lombok.Data;
 
 @Data
 @Entity
-@Table(name="S_EMP")
+@Table(name="S_EMP",
+		uniqueConstraints = {@UniqueConstraint(columnNames = {"NAME", "MAILID"})})
 public class Employee {
 	@Id
+	@Column(length = 7, nullable = false)
 	private Long id;
 	
+	@Column(length = 25, nullable = false)
 	private String name;
 	
+	@Column(length = 8, unique = true)
 	private String mailId;
 	
-	@Column(name="START_DATE")
+	@Column(name="START_DATE", insertable = false)
 	private Date startDate;
 	
+	@Column(length = 25)
 	private String title;
 	
-	@Column(name="DEPT_NAME")
+	@Column(name="DEPT_NAME", length = 30)
 	private String deptName;
 	
+	@Column(precision = 11, scale = 2)
 	private Double salary;
 
-	@Column(name="COMMISSION_PCT")
+	@Column(name="COMMISSION_PCT", precision = 4, scale = 2,
+			columnDefinition = "double CHECK (commission_pct IN (10, 12.5, 15, 17.5, 20))")
 	private Double commissionPct;
 }
